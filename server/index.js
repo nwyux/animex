361320ebdf47f3006abf7ecd1502919e5fad5a83
nwyux <nwyuxtfm@gmail.com>
2024-02-20 00:06:52 +0100

    branch noah

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the Animex Backend!' });
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the Animex Backend!" });
 });
 
-app.get('/', (req, res) => {
-    res.json({ message: 'The API part is on /api! prout' });
+app.get("/", (req, res) => {
+  res.json({ message: "The API part is on /api! prout" });
 });
 
 app.listen(PORT, () => {
