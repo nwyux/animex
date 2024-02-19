de12d0829a7a12a4fe3075cedffcd684b47fea06
nwyux <nwyuxtfm@gmail.com>
2024-02-20 00:21:29 +0100

    characters & characters id

--- server/index.js
+++ server/index.js
@@ -8,8 +8,23 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "The API part is on /api! prout" });
+  res.json({ message: "The API part is on /api!" });
 });
+
+app.get("/api/characters", (req, res) => {
+  res.json([
+    { id: 1, name: "Naruto" },
+    { id: 2, name: "Sasuke" },
+    { id: 3, name: "Sakura" },
+  ]);
+});
+
+app.get("/api/characters/:id", (req, res) => {
+  const id = req.params.id;
+  const character = { id: id, name: "Character Name" };
+  res.json(character);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
